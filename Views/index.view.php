bae32200bcb3bada0b871d09e65375a5a9554e75
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 13:49:04 -0700

    Link button on index.view to out of scope page

--- Views/index.view.php
+++ Views/index.view.php
@@ -9,11 +9,11 @@
 <body>
     <header>
         <img></img> <!-- Logo will go here-->
-        <button>Log in!</button>
+        <a href="outofscope.php">"<button type="button">Log in!</button></a>
     </header>
     <main> 
         <h1>Plan, Play Repeat</h1>
-        <button>Start Planning</button>
+        <button a>Start Planning</button>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <!-- Update with program details and highlights -->
      </main>
 </body>
