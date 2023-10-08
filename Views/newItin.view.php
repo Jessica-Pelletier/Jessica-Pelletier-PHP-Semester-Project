a91ea83ab15db502c38df576755f6839af08932b
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 15:03:34 -0700

    Create and populate form for creating new itinerary on newItin.php page

--- Views/newItin.view.php
+++ Views/newItin.view.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Itinerary</title>
 </head>
+
 <body>
     <?php require('partials/logoheader.php') ?>
-    
+    <main>
+        <h1>New Itinerary</h1>
+        <form action="" method="POST">
+            <p>Where are you going? <input type="text" name="destination"></p>
+            <p>How many days? <input type="text" name="duration"></p> <!--Can I change this to an int?-->
+            <p><a href="home.php">Cancel</a></p>
+            <p><input type="submit" value="Create"></p> <!--Make button link to newdetail.php-->
+        </form>
+    </main>
 </body>
+
 </html>
