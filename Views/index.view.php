4c734be09013224ffe65ffef8553d0d3655e27c3
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 13:34:52 -0700

    Place basic html elements in index.view. header, main, h1, etc

--- Views/index.view.php
+++ Views/index.view.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    
+    <header>
+        <img></img>
+    </header>
+    <main> 
+        <h1></h1>
+        <button></button>
+        <p></p>
+     </main>
 </body>
 </html>
