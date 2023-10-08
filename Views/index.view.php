6c56ae39656066cf07bf2c94f07f0dd1d6038293
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 14:02:20 -0700

    Turn header with logo into a partials file, add require file in index.view to display header

--- Views/index.view.php
+++ Views/index.view.php
@@ -6,11 +6,9 @@
     <title>Index</title> 
 </head>
 <!-- This is the landing page, before log in or account creation. It is the first page that you go to so I called it index. Although im not sure if renaming it to "landing" would be more appropriate. -->
+<?php require('partials/logoheader.php') ?>
 <body>
-    <header>
-        <img src="images/temp-logo-png-transparent.png" width=50 alt="itinerary Logo"></img>
-        <a href="outofscope.php"><button type="button">Log in!</button></a>
-    </header>
+    
     <main> 
         <h1>Plan, Play Repeat</h1>
         <button a>Start Planning</button> 
---
+++ partials/logoheader.php
@@ -0,0 +1,4 @@
+<header>
+<img src="images/temp-logo-png-transparent.png" width=50 alt="itinerary Logo"></img>
+<a href="outofscope.php"><button type="button">Log in!</button></a>
+</header>
