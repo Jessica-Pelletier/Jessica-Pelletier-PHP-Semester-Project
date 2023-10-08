9b27ea12b17ecfd2080f4960bbdef6c25cc0fc91
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 13:39:47 -0700

    Remove controllers folder

--- Views/index.view.php
+++ Views/index.view.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Index</title> 
 </head>
+<!-- This is the landing page, before log in or account creation. It is the first page that you go to so I called it index. Although im not sure if renaming it to "landing" would be more appropriate. -->
 <body>
     <header>
-        <img></img>
+        <img></img> <!-- Logo will go here-->
     </header>
     <main> 
-        <h1></h1>
+        <h1>Plan, Play Repeat</h1>
         <button></button>
         <p></p>
      </main>
