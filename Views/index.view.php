cd7c64939d2c6c597bbcec8b1aeb526a662c7aef
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 13:54:49 -0700

    Add logo to index.view

--- Views/index.view.php
+++ Views/index.view.php
@@ -8,13 +8,14 @@
 <!-- This is the landing page, before log in or account creation. It is the first page that you go to so I called it index. Although im not sure if renaming it to "landing" would be more appropriate. -->
 <body>
     <header>
-        <img></img> <!-- Logo will go here-->
-        <a href="outofscope.php">"<button type="button">Log in!</button></a>
+        <img src="images/temp-logo-png-transparent.png" width=50 alt="itinerary Logo"></img>
+        <a href="outofscope.php"><button type="button">Log in!</button></a>
     </header>
     <main> 
         <h1>Plan, Play Repeat</h1>
-        <button a>Start Planning</button>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <!-- Update with program details and highlights -->
+        <button a>Start Planning</button> 
+        <!-- Update with program details and highlights -->
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
      </main>
 </body>
 </html>
