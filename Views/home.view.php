4ea6110ca7e3973e0571f2f371ebcd958522a4aa
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-08 14:52:04 -0700

    Create and populate links in main, and navigation in aside on home.view

--- Views/home.view.php
+++ Views/home.view.php
@@ -9,12 +9,19 @@
 <?php require('partials/logoheader.php') ?>
     <main>
         <h1>My Intineraries</h1>
-        <a href></a>
-        <a href></a>
+        <a href>View my itineraries</a> <!--TODO: php - add counter for amount of itineraries. -->
+        <a href="newItin.php">Create new</a> 
     </main>
     <aside>
         <nav>
-            
+            <p>Hello, User</p> <!-- TODO: PHP -make dynamic -->
+            <ul>
+                <li><a href="outofscope.php">Profile</a></li>
+                <li><a href="outofscope.php">Itineraries</a></li>
+                <li><a href="outofscope.php">Settings</a></li>
+                <li><a href="outofscope.php">Log out</a></li>
+            </ul>
+
         </nav>
 
     </aside>
