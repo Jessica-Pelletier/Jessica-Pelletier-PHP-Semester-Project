8ee5bbec0ae3f7e9289e2dd158bf4bfe9ae12e03
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 11:16:34 -0700

    Link view pages to stylesheet

--- partials/logoheader.php
+++ partials/logoheader.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="styles/styles.css">
 <header>
 <img class=logo src="images/WanderWiseLogo.png" width=125 alt="itinerary Logo"></img>
 <a href="outofscope.php"><button type="button">Log in!</button></a>
