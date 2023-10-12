41e2669d3b775f6bcb45dcf71d39e860a3e5e0b4
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 15:08:43 -0700

    Remove link default decorations on nav in aside

--- partials/aside.php
+++ partials/aside.php
@@ -5,7 +5,7 @@
                 <li><a href="outofscope.php">Profile</a></li>
                 <li><a href="outofscope.php">Itineraries</a></li>
                 <li><a href="outofscope.php">Settings</a></li>
-                <li><a href="outofscope.php">Log out</a></li>
+                <li class=logout><a href="outofscope.php">Log out</a></li>
             </ul>
 
         </nav>
