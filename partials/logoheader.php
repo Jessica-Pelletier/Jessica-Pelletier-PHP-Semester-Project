eb8d9335f20ef7ff64a4ec68f20175082bcb9450
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 11:03:09 -0700

    Update with new logo in partials header

--- partials/logoheader.php
+++ partials/logoheader.php
@@ -1,4 +1,4 @@
 <header>
-<img src="images/temp-logo-png-transparent.png" width=50 alt="itinerary Logo"></img>
+<img class=logo src="images/WanderWiseLogo.png" width=125 alt="itinerary Logo"></img>
 <a href="outofscope.php"><button type="button">Log in!</button></a>
 </header>
