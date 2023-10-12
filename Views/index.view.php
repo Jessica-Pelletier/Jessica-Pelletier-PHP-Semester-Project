4f8e2f1bda9e7e309e9b23808f18f4277baeb382
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 10:45:59 -0700

    Place Basic html elements on newDetail page. H1, buttons

--- Views/index.view.php
+++ Views/index.view.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
     <title>Index</title> 
 </head>
 <!-- This is the landing page, before log in or account creation. It is the first page that you go to so I called it index. Although im not sure if renaming it to "landing" would be more appropriate. -->
