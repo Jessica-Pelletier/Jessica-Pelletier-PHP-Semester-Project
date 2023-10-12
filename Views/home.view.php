f4c2497d8dcbd413463b247f367830acdfe91227
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 12:06:55 -0700

    Change font family to Montserrat for body elements

--- Views/home.view.php
+++ Views/home.view.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/styles.css">
+    <!--Font import below -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
     <title>Home page</title>
 </head>
 <body>
 <?php require('partials/logoheader.php') ?>
-    <main>
+    <main class=mainhome>
         <h1>My Intineraries</h1>
         <a href>View my itineraries</a> <!--TODO: php - add counter for amount of itineraries. -->
         <a href="newItin.php">Create new</a> 
--- partials/logoheader.php
+++ partials/logoheader.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="styles/styles.css">
-<header>
-<img class=logo src="images/WanderWiseLogo.png" width=125 alt="itinerary Logo"></img>
+
+<header class=header>
+<img src="images/WanderWiseLogo.png" width=125 alt="itinerary Logo"></img>
 <a href="outofscope.php"><button type="button">Log in!</button></a>
 </header>
