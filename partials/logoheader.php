5708a88a0d9298f5f201ea315960787f8a0b372a
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 13:35:05 -0700

    Apply off white background color to all pages for readability

--- partials/logoheader.php
+++ partials/logoheader.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="styles/styles.css">
-
+<!-- TODO: make header responsive to remove login button if not on index page!-->
 <header class=header>
 <img src="images/WanderWiseLogo.png" width=125 alt="itinerary Logo"></img>
 <a href="outofscope.php"><button type="button">Log in!</button></a>
