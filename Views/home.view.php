8604df5c7e232f705952f57e22745e9ec0ec52f0
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 14:25:49 -0700

    Take the aside element out of home page and place in partials folder, replace with require link

--- Views/home.view.php
+++ Views/home.view.php
@@ -21,20 +21,8 @@
         <a href="newItin.php">Create new</a> 
     </main>
 
+    <?php require('partials/aside.php')?>
     
-    <aside class=asideitem2>
-        <nav>
-            <p>Hello, User</p> <!-- TODO: PHP -make dynamic -->
-            <ul>
-                <li><a href="outofscope.php">Profile</a></li>
-                <li><a href="outofscope.php">Itineraries</a></li>
-                <li><a href="outofscope.php">Settings</a></li>
-                <li><a href="outofscope.php">Log out</a></li>
-            </ul>
-
-        </nav>
-
-    </aside>
 
 </div>
 </body>
---
+++ partials/aside.php
@@ -0,0 +1,13 @@
+<aside class=asideitem2>
+        <nav>
+            <p>Hello, User</p> <!-- TODO: PHP -make dynamic -->
+            <ul>
+                <li><a href="outofscope.php">Profile</a></li>
+                <li><a href="outofscope.php">Itineraries</a></li>
+                <li><a href="outofscope.php">Settings</a></li>
+                <li><a href="outofscope.php">Log out</a></li>
+            </ul>
+
+        </nav>
+
+    </aside>
