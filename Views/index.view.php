afa1ebd25099cf44027208e922aee4b06abfb7c1
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-12 12:50:46 -0700

    Add styling to main cta button on index

--- Views/index.view.php
+++ Views/index.view.php
@@ -13,7 +13,7 @@
     
     <main class=index> 
         <h1>Plan, Play Repeat</h1>
-        <a href="onboard.php"><button>Start Planning</button></a>
+        <a href="onboard.php"><button class=indexcta>Start Planning</button></a>
         <!-- Update with program details and highlights -->
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
      </main>
