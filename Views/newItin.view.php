bdad920df3481182b52139e6805d9bbdaa035421
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-13 13:55:50 -0700

    Remove black border on new itinerary , resize height and width

--- Views/newItin.view.php
+++ Views/newItin.view.php
@@ -15,8 +15,10 @@
         <h1>New Itinerary</h1>
         
         <form action="" method="POST">
+            <div class=form>
             <p>Where are you going? <input type="text" name="destination"></p>
-            <p>How many days? <input type="text" name="duration"></p> <!--Can I change this to an int?-->
+            <p>How many days? <input type="text" name="duration"></p> 
+            </div><!--Can I change this to an int?-->
             <button class=cancel><a href="home.php">Cancel</a></button>
             <!--<p><a href="newdetail.php"><input type="submit" value="Create"></a></p>--> <!--Make button link to newdetail.php-->
             <button class=create><a href="newdetail.php">Create</a></button>
