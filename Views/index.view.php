c72288873dacf094406daa012d0fbb4ffbc4f5a3
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-13 10:05:49 -0700

    Format Styling sheet

--- Views/index.view.php
+++ Views/index.view.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/styles.css">
-   
-    <title>Index</title> 
+
+    <title>Index</title>
 </head>
 <!-- This is the landing page, before log in or account creation. It is the first page that you go to so I called it index. Although im not sure if renaming it to "landing" would be more appropriate. -->
 <?php require('partials/logoheader.php') ?>
-<body >
-    
-    <main class=index> 
-        <h1>Plan, Play, Repeat</h1>
-        <h2 class=indexh2>Make things simple with the Wander Wise Itinerary planner<h2>
-        <a href="onboard.php"><button class=indexcta>Start Planning</button></a>
-        <!-- Update with program details and highlights -->
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
-     </main>
+<body>
+
+    <main class=index>
+        <section>
+            <h1>Plan, Play, Repeat</h1>
+            <h2 class=indexh2>Make things simple with the Wander Wise Itinerary planner<h2>
+                    <a href="onboard.php"><button class=indexcta>Start Planning</button></a>
+
+        </section>
+        <section class=indexcontent><!-- Update with program details and highlights -->
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
+    </main>
 </body>
+
 </html>
--- Views/newdetail.view.php
+++ Views/newdetail.view.php
@@ -8,11 +8,13 @@
 </head>
 <?php require('partials/logoheader.php') ?>
 <body>
-    <main>
+    <main class=newdetail>
+        <div>
         <h1>NAME OF LOCATION</h1> <!-- TODO: Php make responsive to location selected by user-->
         <div>Intinerary Block</div> <!--Make responsive-->
         <button><a href="newItin.php">Back</a></button>
         <button><a href="home.php">Save</a></button>
+</div>
 
 
 
