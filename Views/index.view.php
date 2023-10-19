4fe1132dd21276c4d114d4722f5759657f591ecc
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-19 09:08:43 -0700

    Add basic outline of footer without styling

--- Views/index.view.php
+++ Views/index.view.php
@@ -12,20 +12,22 @@
 <?php require('partials/logoheader.php') ?>
 
 <body>
+    <main>
+        <div class=indexpage>
 
-    <main class=index>
-    
-        <section class=indexhero>
-            <h1>Plan, Play, Repeat</h1>
-            <h2 class=indexh2>Make things simple with the Wander Wise Itinerary planner</h2>
-                    <a href="onboard.php"><button class=indexcta>Start Planning</button></a>
+            <section class=indexhero>
+                <h1>Plan, Play, Repeat</h1>
+                <h2 class=indexh2>Make things simple with the Wander Wise Itinerary planner</h2>
+                <a href="onboard.php"><button class=indexcta>Start Planning</button></a>
 
+        </div>
         </section>
-     <!--<section>
+        <section>
+            <h2>The APP</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </section> -->
-
+        </section> 
     </main>
+     <?php require('partials/footer.php') ?> 
 </body>
 
 </html>
--- Views/onboard.view.php
+++ Views/onboard.view.php
@@ -15,16 +15,22 @@
         <h1>Create an account</h1>
         <!--TODO: Add action to form -->
         <form action="" method="POST">
-            <p>Name: <input type="text" name="name"></p>
-            <p>Email: <input type="text" name="email"></p>
-            <p>Password: <input type="text" name="password"></p>
-            <p><input type="checkbox" name="terms" value="true">
-                I agree to the terms and conditions.</p>
+            <label for="name">Name:</label>
+            <input type="text" name="name">
+            <label for="email">Email:</label>
+            <input type="text" name="email">
+            <label for="password">Password</label>
+            <input type="text" name="password">
+            <input type="checkbox" name="terms" value="true">
+            <label for="terms">I agree to the terms and conditions.</label>
+            
+                
             <!-- <a href="onboard.php"><p><input type="submit" value="Create"></p></a> TODO:Make link to onboard -->
         </form>
         <button><a href="home.php">Sign up</a></button> <!-- This is a sudo button for temp user flow ONLY, please delete once form button is properly linked-->
 </div>
     </main>
+<?php require('partials/footer.php') ?>
 
 </body>
 
