55a9658b1a2b4ba40ab057c76f9c29fe2acd6eef
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-25 11:33:04 -0700

    Make each input on the onboard form required. Have the submit button actually submit the form

--- Views/onboard.view.php
+++ Views/onboard.view.php
@@ -14,23 +14,26 @@
         <div class=onboardform>
         <h1>Create an account</h1>
         <!--TODO: Add action to form -->
-        <form action="" method="POST">
+        <form action="home.php" method="POST">
             <label for="name">Name:</label>
-            <input type="text" name="name">
+            <input type="text" name="name" required>
+
             <label for="email">Email:</label>
-            <input type="text" name="email">
+            <input type="email" name="email" required>
+
             <label for="password">Password</label>
-            <input type="text" name="password">
+            <input type="password" name="password">
+
             <label for="password">Re-enter Password</label>
-            <input type="text" name="password">
+            <input type="password" name="password" required>
             
-            <input type="checkbox" name="terms" value="true">
+            <input type="checkbox" name="terms" value="true" required>
             <label for="terms">I agree to the <a href="outofscope.php">terms and conditions.</a></label>
 
-                
-            <!-- <a href="onboard.php"><p><input type="submit" value="Create"></p></a> TODO:Make link to onboard -->
+            <a href="home.php"><input type="submit" value="Create"></a> 
+            
         </form>
-        <button><a href="home.php">Sign up</a></button> <!-- This is a sudo button for temp user flow ONLY, please delete once form button is properly linked-->
+        <!-- <button><a href="home.php">Sign up</a></button> This is a sudo button for temp user flow ONLY, please delete once form button is properly linked -->
 </div>
     </main>
 <?php require('partials/footer.php') ?>
