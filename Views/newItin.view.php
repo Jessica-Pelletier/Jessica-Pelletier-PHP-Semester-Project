62e299e989b97a5ad88017169e7253964e67fdeb
Jessica-Pelletier <jj.pelletier@outlook.com>
2023-10-25 10:36:29 -0700

    Fix error where itinerary form lacked labels

--- Views/newItin.view.php
+++ Views/newItin.view.php
@@ -12,20 +12,22 @@
     <?php require('partials/logoheader.php') ?>
     <main class=newitin>
         <div class=newitindiv>
-        <h1>New Itinerary</h1>
-        
-        <div class=form>
-        <form action="" method="POST">
-            
-            <p>Where are you going? <input type="text" name="destination"></p>
-            <p>How many days? <input type="text" name="duration"></p> 
+            <h1>New Itinerary</h1>
+
+            <div class=form>
+                <form action="" method="POST">
+
+                <label for="destination">Where are you going?</label><input type="text" name="destination">
+                <label for="duration">How many days?</label><input type="text" name="duration">
+
+                
             </div><!--Can I change this to an int?-->
             <button class=cancel><a href="home.php">Cancel</a></button>
             <!--<p><a href="newdetail.php"><input type="submit" value="Create"></a></p>--> <!--Make button link to newdetail.php-->
             <button class=create><a href="newdetail.php">Create</a></button>
-        </form>
+            </form>
         </div>
-        
+
     </main>
     <?php require('partials/footer.php') ?>
 </body>
